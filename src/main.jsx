ec717c64a8d5025f84a560ed0b38da45a26cab4a
saran <95495897+2000030236@users.noreply.github.com>
2025-09-17 14:48:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import this
+import { HashRouter } from 'react-router-dom'; // Import this
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/employee.app">  // No trailing slash
+    <HashRouter>  // No trailing slash
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
