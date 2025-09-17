da5ce8b303955e78ae5331c8f9fd63224f55e3af
saran <95495897+2000030236@users.noreply.github.com>
2025-09-17 14:31:19 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Import this
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/employee.app/">  // Add this with your repo name
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
